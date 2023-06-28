62bb836aa5e1b5b2ed478c80b90a045089769395
archiesevillano <archie.sevillano29@gmail.com>
2023-06-28 12:33:29 +0800

    Direct Link and Alternative Link for CV added

--- src/components/AppContent/AppContent.js
+++ src/components/AppContent/AppContent.js
@@ -7,18 +7,11 @@ import EmailIcon from '@mui/icons-material/Email';
 import "./AppContent.css";
 import { useMemo, useState } from "react";
 import SwiperSection from "../SwiperSection/SwiperSection";
-import FileDownload from "js-file-download";
 import Axios from "axios";
 
 const AppContent = () => {
 
-    // download resume file from firebase
-    const downloadResume = async () => {
-        const response = await Axios.get("http://localhost:3001/file/download/resume.pdf", { responseType: 'blob' });
-        const data = await response.data;
-        console.log(data);
-        FileDownload(data, "ACS_CV.pdf");
-    }
+
 
     const currentTheme = useTheme();
     const [hasStarted, setStartStatus] = useState(false);
@@ -65,10 +58,7 @@ const AppContent = () => {
                             <Typography variant="body1" maxWidth="300px" sx={{ marginTop: "10px", fontSize: "1rem" }}>
                                 I am a passionate Web Developer who transform my Ideas into digital reality with Creativity and Technical Expertise
                             </Typography>
-                            <Box sx={{ display: "flex", gap: "20px", margin: "20px 0" }}>
-                                <Button variant="contained" color="success" sx={{ width: "150px", color: "white" }} startIcon={<DownloadIcon />} onClick={downloadResume}>Download CV</Button>
-                                <Button variant="contained" color="secondary" sx={{ width: "150px", color: "white" }} startIcon={<EmailIcon />}>Message me</Button>
-                            </Box>
+
                         </Box>
                     </div>
                     <div className="right">
@@ -86,10 +76,6 @@ const AppContent = () => {
                     <Typography variant="body1" maxWidth="300px" sx={{ marginTop: "10px", fontSize: "1rem", color: "white", }}>
                         I am a passionate Web Developer who transform my Ideas into digital reality with Creativity and Technical Expertise
                     </Typography>
-                    <Box sx={{ display: "flex", gap: "20px", margin: "20px 0", flexDirection: "column" }}>
-                        <Button variant="contained" color="success" sx={{ flexGrow: 1, fontSize: "1rem", color: "white" }} startIcon={<DownloadIcon />} onClick={downloadResume}>Download CV</Button>
-                        <Button variant="contained" color="secondary" sx={{ flexGrow: 1, color: "white", fontSize: "1rem" }} startIcon={<EmailIcon />}>Message me</Button>
-                    </Box>
                 </Box>
                 <img src={"https://firebasestorage.googleapis.com/v0/b/archs-baedb.appspot.com/o/profile.png?alt=media&token=81b46136-89f4-4e90-bfa8-d8151b6989f7"} className="profile-pic" />
             </Container>
--- src/components/CVSection/CVSection.js
+++ src/components/CVSection/CVSection.js
@@ -1,29 +1,58 @@
 import { Container, Typography, Box, Button, useTheme } from "@mui/material";
 import "./CVSection.css";
 import { Download } from "@mui/icons-material";
+import FileDownload from 'js-file-download';
+import Axios from 'axios';
 
 const CvSection = () => {
 
     const currentTheme = useTheme();
+    const gdriveIcon = "https://upload.wikimedia.org/wikipedia/commons/1/12/Google_Drive_icon_%282020%29.svg";
+
+    // download resume file from firebase
+    const downloadResume = async () => {
+        const response = await Axios.get("http://localhost:3001/file/download/resume.pdf", { responseType: 'blob' });
+        const data = await response.data;
+        console.log(data);
+        FileDownload(data, "ACS_CV_DD.pdf"); //DD stands for Direct Download
+    }
+
+    //download file via google drive alternative link
+    const alternativeDownload = async () => {
+        const response = await Axios.get("http://localhost:3001/file/download/alternative-download-cv", { responseType: 'blob' });
+        const data = await response.data;
+        console.log(data);
+        FileDownload(data, "ACS_CV_AD.pdf"); //AD stands for Direct Download
+    }
 
     return (
-        <Container sx={{ maxWidth: "100%!important", margin: 0, height: "50vh", display: "flex", justifyContent: "space-evenly", alignItems: "center", backgroundColor: currentTheme.palette.primary.main }} disableGutters>
+        <Container sx={{ maxWidth: "100%!important", margin: 0, height: "50vh", display: "flex", justifyContent: "space-evenly", alignItems: "center", backgroundColor: currentTheme.palette.primary.main, background: "linear-gradient(13deg, rgba(233,94,59,1) 19%, rgba(236,166,148,1) 100%)" }} disableGutters>
             <Box sx={{ maxWidth: "500px", minWidth: "300px", flexGrow: 1, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start" }}>
+                <Typography variant="subtitle1" sx={{ fontSize: "1.4rem", fontWeight: "bold", color: "black", opacity: "0.9", transform: "translateY(10px)" }}>Are you a recruiter?</Typography>
                 <Typography variant="subtitle1" sx={{ fontSize: "2rem", fontWeight: "bold" }}>Download my latest CV</Typography>
-                <Typography variant="body1" sx={{ color: "white" }}>
-                    Review and obtain the latest version of my CV.  I am excited to discuss further how my skills can bring value to your organization. Send me an email here.
+                <Typography variant="body1" fontSize="0.8rem" sx={{ color: "white" }}>
+                    Review and obtain the latest version of my CV.  I am excited to discuss further how my skills can bring value to your organization.
                 </Typography>
             </Box>
             <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", gap: "20px" }}>
-                <Button variant="outlined" className="download__button" sx={{ textTransform: "capitalize!important", letterSpacing: "2px", fontSize: "1.2rem", width: "250px", borderColor: "white", color: "white" }}  >
-                    <Download sx={{ marginRight: "15px" }} />
-                    Download CV
+                <Button variant="outlined" onClick={downloadResume} className="download__button" sx={{ width: "250px", borderColor: "white", color: "white", backgroundColor: "var(--secondary)" }}  >
+                    <Download sx={{ marginRight: "15px", fontSize: "40px" }} />
+                    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start" }}>
+                        <Typography variant="body1" fontSize="0.7rem">Direct link</Typography>
+                        <Typography variant="body1" fontSize="1.2rem" fontWeight="bold" sx={{ textTransform: "capitalize" }}>Download CV</Typography>
+                    </Box>
                 </Button>
-                <Button variant="text" sx={{ textTransform: "capitalize!important", letterSpacing: "2px", fontSize: "1.2rem", width: "250px", borderColor: "white", color: "white" }}  >
-                    Alternative Link
+                <Button variant="outlined" onClick={alternativeDownload} className="download__button" sx={{ width: "250px", backgroundColor: "rgb(236, 236, 236)" }}  >
+                    <Box sx={{ marginRight: "15px", width: "16%", transform: "translateY(5px)" }}>
+                        <img src={gdriveIcon} style={{ width: "100%", height: "100%", objectFit: "contain" }} alt="Icon" />
+                    </Box>
+                    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start" }}>
+                        <Typography variant="body1" fontSize="0.7rem" color="secondary" >Alternative link</Typography>
+                        <Typography variant="body1" fontSize="1.2rem" fontWeight="bold" sx={{ textTransform: "capitalize", color: "var(--secondary)" }}>Google Drive</Typography>
+                    </Box>
                 </Button>
-            </Box>
-        </Container>
+            </Box >
+        </Container >
     );
 }
 
