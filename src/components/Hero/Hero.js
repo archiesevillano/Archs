b5b55934339163e70ed1893a69861566c75ac7d9
archiesevillano <archie.sevillano29@gmail.com>
2023-06-28 17:19:40 +0800

    Social Icons added to Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,7 @@ import "./Hero.css";
 import { useTheme, Paper, Container, Box, Typography } from '@mui/material';
 import React, { useMemo, useState, useRef } from 'react';
 import EmailIcon from '@mui/icons-material/Email';
+import Socials from "../Socials/Socials";
 
 const Hero = () => {
 
@@ -37,21 +38,24 @@ const Hero = () => {
             <div className="hero__hello_container" ref={greetingRef} onClick={toggleGreeting}>
                 <span className="hello_container__hello">He<br />llo</span>
             </div>
-            <Container disableGutters className="heroSection__content" sx={{ maxWidth: "95%!important", margin: "0", justifyContent: { xl: "space-center", sm: "baseline" }, alignItems: { xl: "center", sm: "center" }, flexDirection: { xl: "row", sm: "column" } }}>
+            <Container disableGutters className="heroSection__content" sx={{ maxWidth: "95%!important", margin: "0", justifyContent: { xl: "space-evenly", sm: "baseline" }, alignItems: { xl: "center", sm: "center" }, flexDirection: { xl: "row", sm: "column" } }}>
                 <div className="content__left">
-                    <Box>
+                    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "flex-start", flexDirection: "column" }}>
                         <Typography variant="subtitle1" sx={{ fontSize: "1.5rem", textTransform: "uppercase", letterSpacing: "3px" }}>Getting To Know</Typography>
                         <Typography variant="subtitle1" fontWeight="bold" sx={{ fontSize: "2.5rem", margin: 0 }}>Full-Stack Web Developer</Typography>
                         <Typography variant="body1" maxWidth="300px" sx={{ fontSize: "1rem" }}>
                             I am a passionate Web Developer who transform my Ideas into digital reality with Creativity and Technical Expertise
                         </Typography>
-                        <button class="playstore-button" href="#">
-                            <EmailIcon className="icon" />
-                            <span class="texts">
-                                <span class="text-1">GET IN TOUCH</span>
-                                <span class="text-2">Message me</span>
-                            </span>
-                        </button>
+                        <Box sx={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+                            <button class="playstore-button" href="#">
+                                <EmailIcon className="icon" />
+                                <span class="texts">
+                                    <span class="text-1">GET IN TOUCH</span>
+                                    <span class="text-2">Message me</span>
+                                </span>
+                            </button>
+                            <Socials />
+                        </Box>
                     </Box>
                 </div>
                 <div className="content__right">
