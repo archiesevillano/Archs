7ad5be81023b3c0e122ad5f9c2cb13060597f5b1
archiesevillano <archie.sevillano29@gmail.com>
2023-07-03 12:12:10 +0800

    installed react-scrollspy

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -19,6 +19,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Scrollspy from 'react-scrollspy';
 
 
 const AppHeader = () => {
