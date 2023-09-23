3277ea0e2776b724062ba748e08db3a1f3afcbdb
archiesevillano <archie.sevillano29@gmail.com>
2023-09-23 12:02:27 +0800

    Added responsiveness

--- src/components/MissingPage.tsx
+++ src/components/MissingPage.tsx
@@ -20,15 +20,15 @@ const MissingPage = () => {
     }
 
     return (
-        <div className="missingPageComponent flex flex-col items-center justify-center">
-            <h1 className="font-bold text-5xl mb-4">Page not found</h1>
-            <p className="text-sm opacity-80 font-quicksand">Seems like the page that you&apos;re trying to visit does not exist.</p>
+        <div className="missingPageComponent flex flex-col sm:items-center items-start justify-center sm:px-0 px-10">
+            <i className="fi fi-br-sensor-alert inline-block translate-y-[3px] text-5xl"></i>
+            <h1 className="font-bold text-5xl mb-4 w-full">Page not found</h1>
+            <p className="text-sm opacity-80 font-quicksand mb-3">Seems like the page that you&apos;re trying to visit does not exist.</p>
             <p className="text-sm opacity-80 font-quicksand mb-5">Try to check if you have entered a correct URL.</p>
             <button type="button" onClick={handleClick} className="border-2 px-4 py-2 font-quicksand font-bold mb-[100px] rounded-lg">
                 <i className="fi fi-rr-arrow-small-left translate-y-[3px] me-3 inline-block"></i>
                 Go back
             </button>
-            <Brand />
         </div>
     );
 }
